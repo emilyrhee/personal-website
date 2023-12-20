5c9ed09567b0d67df6b8c3224ad42727a67fbbec
emilyrhee <emilyrhee99@gmail.com>
2023-12-20 11:56:32 -0500

    made beige custom color as bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'beige': '#F8F0DC'
+      }
+    },
   },
   plugins: [],
 }

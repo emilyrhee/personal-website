4afac7c355a3aaf3ce9b664e6fbc3d794adfcd51
emilyrhee <emilyrhee99@gmail.com>
2023-12-19 20:21:45 -0500

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<body>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Poop poop
+</button>
+
+</body>

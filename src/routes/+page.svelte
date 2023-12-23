6cdd4c2a823a58f5575c3f0e7bcc92ca3f17d27c
emilyrhee <emilyrhee99@gmail.com>
2023-12-23 17:55:04 -0500

    added subheader with padding adjustments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <head>
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
-  <link href="https://fonts.googleapis.com/css2?family=Coiny&family=JetBrains+Mono&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+<link href="https://fonts.googleapis.com/css2?family=Coiny&family=JetBrains+Mono:wght@400;600&display=swap" rel="stylesheet">
 
 </head>
 
@@ -14,11 +14,13 @@
 
 <body class="bg-beige">
 
-  <div class='px-56'>
-    <h1 class="pt-8 text-brown text-6xl font-coiny">Hi, I'm Emily Rhee!</h1>
+  <div class='px-60'>
+    <h1 class="pt-8 py-4 text-brown text-6xl font-coiny">Hi, I'm Emily Rhee!</h1>
+    <h3 class="text-brown text-3xl font-jetbrains-bold">Software developer</h3>
     <p class="py-4 text-brown font-jetbrains">
-      I'm a passionate software developer pursuing a bachelor's degree in 
-      computer science at Central Connecticut State University.
+      I am pursuing a bachelor's degree in computer science at Central 
+      Connecticut State University. My main interests in programming are game 
+      dev, web dev, and embedded systems.
     </p>
 
     <button class="bg-light-brown text-beige font-jetbrains py-2 px-4 rounded">

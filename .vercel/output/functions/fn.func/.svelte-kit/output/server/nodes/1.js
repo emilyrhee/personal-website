d5eec4f56c8f6280bfc0a247236a3313a01ed9d3
emilyrhee <emilyrhee99@gmail.com>
2024-07-07 17:32:06 -0400

    Revert "edited writing on home page"
    This reverts commit 5e0e923fcffc8d0320beb897150b480e3289c01c.

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.DbRXMqFC.js","_app/immutable/chunks/scheduler.BvLojk_z.js","_app/immutable/chunks/index.g6DG62DB.js","_app/immutable/chunks/entry.BGNn2PY2.js"];
-export const stylesheets = [];
-export const fonts = [];

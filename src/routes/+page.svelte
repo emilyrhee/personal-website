006ca4cda2052893c95b3f61832b16f92ddf254a
emilyrhee <emilyrhee99@gmail.com>
2024-07-10 22:12:07 -0400

    made homepage more responsive

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,30 +8,32 @@
   import { NavBar } from '$lib'
 </script>
 
-<body class="bg-beige px-4 sm:px-6 md:px-8 lg:px-16 xl:px-80">
+<body class="bg-beige px-4 sm:px-6 md:px-8 lg:px-16 xl:px-32 2xl:px-48">
 
   <NavBar />
 
-  <div class="py-16"></div>
+  <div class="py-4 sm:py-8 md:py-16"></div>
 
-  <h1 class="pt-8 py-4 text-brown text-6xl font-coiny">Hi, I'm Emily Rhee!</h1>
-  <h3 class="text-brown text-3xl font-jetbrains-bold">Software developer</h3>
-  <p class="py-4 text-lg text-brown font-jetbrains">
-    I am pursuing a bachelor's degree in computer science at Central 
-    Connecticut State University, and hope to continue in grad school. My main interest in tech is in game 
-    development, but I also dabble in web dev, systems programming, and embedded systems.
-  </p>
-
-  <a href="https://github.com/emilyrhee" target="_blank">
+  <div class="w-full sm:w-3/4 md:w-2/3 lg:w-1/2 xl:w-7/12 2xl:w-7/12 mx-auto">
+    <h1 class="pt-8 py-4 text-brown text-6xl font-coiny">Hi, I'm Emily Rhee!</h1>
+    <h3 class="text-brown text-3xl font-jetbrains-bold">Software developer</h3>
+    <p class="py-4 text-lg text-brown font-jetbrains">
+      I am pursuing a bachelor's degree in computer science at Central 
+      Connecticut State University, and hope to continue in grad school. My main interest in tech is in game 
+      development, but I also dabble in web dev, systems programming, and embedded systems.
+    </p>
+    
+    <a href="https://github.com/emilyrhee" target="_blank">
     <button class="bg-light-brown text-lg text-beige font-jetbrains py-2 px-4 rounded">
       GitHub
     </button>
-  </a>
+    </a>
   
-  <a href="https://www.linkedin.com/in/emily-rhee-055731167/" target="_blank">
-    <button class="bg-green text-lg text-beige font-jetbrains py-2 px-4 rounded">
-      LinkedIn
-    </button>
-  </a>
+    <a href="https://www.linkedin.com/in/emily-rhee-055731167/" target="_blank">
+      <button class="bg-green text-lg text-beige font-jetbrains py-2 px-4 rounded">
+        LinkedIn
+      </button>
+    </a>
+  </div>
 
 </body>

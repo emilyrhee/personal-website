f739ebb96fe53f1886d7b1a5c590436cf112a64f
emilyrhee <emilyrhee99@gmail.com>
2024-07-17 17:01:06 -0400

    added GPT project to Projects page

---
+++ src/lib/Bolts.svelte
@@ -0,0 +1,4 @@
+<div id="top-left-circle" class="absolute top-4 left-4 w-4 h-4 bg-beige rounded-full transform -translate-x-1/2 -translate-y-1/2"></div>
+<div id="top-right-circle" class="absolute top-4 right-4 w-4 h-4 bg-beige rounded-full transform translate-x-1/2 -translate-y-1/2"></div>
+<div id="bottom-left-circle" class="absolute bottom-4 left-4 w-4 h-4 bg-beige rounded-full transform -translate-x-1/2 translate-y-1/2"></div>
+<div id="bottom-right-circle" class="absolute bottom-4 right-4 w-4 h-4 bg-beige rounded-full transform translate-x-1/2 translate-y-1/2"></div>
---
+++ src/lib/RoomTour.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import Bolts from '$lib/Bolts.svelte'
+</script>
+
+<div class="relative bg-green text-lg text-beige font-jetbrains p-12 rounded">
+    <Bolts />
+    
+    <h2 class="text-beige text-4xl font-coiny">Room Tour</h2>
+    <p class="py-2 text-beige font-jetbrains">
+    A small game where I give a tour of my bedroom. The player walks into
+    furniture to display a factoid about it. Made using the JavaFX library
+    to assist with creating GUI.
+    </p>
+    <!-- <img class="mx-auto py-3" alt="Room tour program preview" src={roomtour} /> -->
+    <a href="https://github.com/emilyrhee/room-tour" target="_blank" class="underline">Code</a>
+</div>

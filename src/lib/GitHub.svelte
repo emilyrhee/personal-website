91f0a0a0e7d4a6752b80485dabf861e9b7a66c70
emilyrhee <emilyrhee99@gmail.com>
2024-07-22 16:40:51 -0400

    made buttons clicky :3

--- src/lib/GitHub.svelte
+++ src/lib/GitHub.svelte
@@ -1,9 +1,25 @@
+<script>
+  let topClass = 'top-0';
+
+  function handleMouseDown() {
+    topClass = 'top-2';
+  }
+
+  function handleMouseUp() {
+    topClass = 'top-0';
+  }
+</script>
+
 <button class="bg-medium-brown text-lg text-beige font-jetbrains py-2 px-4 rounded relative transform translate-y-2">
   GitHub
 </button>
 
 <a href="https://github.com/emilyrhee" target="_blank">
-  <button class="bg-light-brown text-lg text-beige font-jetbrains py-2 px-4 rounded absolute top-0 left-0">
+  <button
+    on:mousedown={handleMouseDown}
+    on:mouseup={handleMouseUp}
+    class={`bg-light-brown text-lg text-beige font-jetbrains py-2 px-4 rounded absolute left-0 ${topClass}`}
+  >
     GitHub
   </button>
 </a>
--- src/lib/LinkedIn.svelte
+++ src/lib/LinkedIn.svelte
@@ -1,9 +1,25 @@
-<button class="bg-dark-green text-lg text-beige font-jetbrains py-2 px-4 rounded absolute top-2 left-0">
+<script>
+  let topClass = 'top-0';
+
+  function handleMouseDown() {
+    topClass = 'top-2';
+  }
+
+  function handleMouseUp() {
+    topClass = 'top-0';
+  }
+</script>
+
+<button class="bg-dark-green text-lg text-beige font-jetbrains py-2 px-4 rounded relative transform translate-y-2">
   LinkedIn
 </button>
 
-<a href="https://www.linkedin.com/in/emily-rhee-055731167/" target="_blank">
-  <button class="bg-green text-lg text-beige font-jetbrains py-2 px-4 rounded relative transform">
+<a href="https://github.com/emilyrhee" target="_blank">
+  <button
+    on:mousedown={handleMouseDown}
+    on:mouseup={handleMouseUp}
+    class={`bg-green text-lg text-beige font-jetbrains py-2 px-4 rounded absolute left-0 ${topClass}`}
+  >
     LinkedIn
   </button>
 </a>

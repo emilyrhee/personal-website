e2247b193e9e1965216062da3fd9a9812cabda17
emilyrhee <emilyrhee99@gmail.com>
2024-07-31 14:20:14 -0400

    click more to see more ; )

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,7 +1,24 @@
 <script lang="ts">
-  import Bolts from '$lib/Bolts.svelte'
+  import Bolts from '$lib/Bolts.svelte';
 
   export let project;
+
+  let description: string = "";
+  let max: number = 75;
+  let isDescriptionFull: boolean = false;
+  $: {
+    if (isDescriptionFull) {
+      description = project.description;
+    } else {
+      if (project.description.length > max) {
+        description = project.description.substring(0, max);
+      }
+    }
+  }
+
+  function showDescription() {
+    isDescriptionFull = true;
+  }
 </script>
 
 <div class="relative {project.color} text-lg text-beige font-jetbrains p-12 rounded-md">
@@ -9,7 +26,14 @@
     
     <h2 class="text-beige text-4xl font-coiny">{project.name}</h2>
     <p class="py-2 text-beige font-jetbrains">
-      {project.description}
+      {description} 
+      {#if !isDescriptionFull}
+        <span>
+          <button class="underline" on:click={showDescription}>
+            ...more
+          </button>
+        </span>
+      {/if}
     </p>
     <a href="{project.link}" target="_blank" class="underline">GitHub</a>
 </div>

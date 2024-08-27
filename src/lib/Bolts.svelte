389907fd06c5ede2c6b97463962194ac585ddb77
emilyrhee <emilyrhee99@gmail.com>
2024-08-26 23:04:44 -0400

    made bolts unselectable

--- src/lib/Bolts.svelte
+++ src/lib/Bolts.svelte
@@ -1,15 +1,15 @@
 <div id="top-left-circle" class="absolute top-4 left-4 w-4 h-4 bg-beige rounded-full transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-  <span class="text-dark-beige font-bold">+</span>
+  <span class="text-dark-beige font-bold select-none">+</span>
 </div>
 
 <div id="top-right-circle" class="absolute top-4 right-4 w-4 h-4 bg-beige rounded-full transform translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-  <span class="text-dark-beige font-bold">+</span>
+  <span class="text-dark-beige font-bold select-none">+</span>
 </div>
 
 <div id="bottom-left-circle" class="absolute bottom-4 left-4 w-4 h-4 bg-beige rounded-full transform -translate-x-1/2 translate-y-1/2 flex items-center justify-center">
-  <span class="text-dark-beige font-bold">+</span>
+  <span class="text-dark-beige font-bold select-none">+</span>
 </div>
 
 <div id="bottom-right-circle" class="absolute bottom-4 right-4 w-4 h-4 bg-beige rounded-full transform translate-x-1/2 translate-y-1/2 flex items-center justify-center">
-  <span class="text-dark-beige font-bold">+</span>
+  <span class="text-dark-beige font-bold select-none">+</span>
 </div>

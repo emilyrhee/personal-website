5bc32e4fd90b3bbd761de616b77d6fba44606d4c
emilyrhee <emilyrhee99@gmail.com>
2025-09-06 15:52:25 -0400

    hardcoded one project. make this into a component pls!

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,5 +11,13 @@
 >
   <div class="flex flex-col items-start">
     <TerminalHeader text="Projects" />
+
+    <div class="flex items-center">
+      <img src="/btr.png" class="h-36 pt-4" alt="Bridging the Rift">
+      <div class="flex flex-col pl-2">
+        <h6 class="text-white">Bridging the Rift</h6>
+        <p class="text-white">A 2D platformer game</p>
+      </div>
+    </div>
   </div>
 </div>

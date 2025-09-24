f55dd1b90359bfb7e6aebdbbe222c1383c083549
emilyrhee <emilyrhee99@gmail.com>
2025-09-23 20:38:00 -0400

    made `TerminalHeader` colors customizable

--- src/lib/components/TerminalHeader.svelte
+++ src/lib/components/TerminalHeader.svelte
@@ -2,14 +2,17 @@
   @component
   Puts an emerald '>' at the beginning of text and applies white to the rest.
 
-  **Parameters**  
+  **Parameters**
   `text` - Text to go in header.
+  `color` - (Optional) Color class for the '>'. Defaults to text-emerald-500.
 -->
 <script>
   export let text;
+  export let textColor = "text-neutral-200";
+  export let arrowColor = 'text-emerald-500'; // Default color
 </script>
 
-<h3 class="text-neutral-200">
-  <span class="text-emerald-500 font-extrabold">> </span>
+<h3 class="{textColor}">
+  <span class="{arrowColor} font-extrabold">> </span>
   {text}
 </h3>
